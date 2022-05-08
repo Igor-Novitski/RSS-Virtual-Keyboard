5b5607cfcded39da497c4bbb28804d921ba2a295
Igor-Novitski <pro100nke@gmail.com>
2022-05-08 23:15:32 +0300

    feat: add capslk functional

--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -29,11 +29,10 @@ keys.forEach((item) => { item.addEventListener('mouseout', (changeButtonUp)) });
 
 const rows = document.querySelectorAll('.row');
 
-
-let lang = 'en';
+if (!localStorage.getItem('lang')) localStorage.setItem('lang', 'en');
 
 function firstFillKeys() {
-   if (lang === 'en') {
+   if (localStorage.lang === 'en') {
       for (let i = 0; i < rows.length; i++) {
          for (let j = 0; j < rows[i].children.length; j++) {
             rows[i].children[j].innerHTML = dataButtons[i][j].key.en;
@@ -59,13 +58,45 @@ const langK = document.querySelector('.Lang');
 
 //change lang
 
+
+
 function changeLang() {
-   (lang === 'en') ? lang = 'ru' : lang = 'en';
-   firstFillKeys();
+   (localStorage.lang === 'en') ? localStorage.lang = 'ru' : localStorage.lang = 'en';
+   fillKeys();
 }
 
 langK.addEventListener('click', changeLang);
 
 
 
-console.log(rows[4].children);
+function changeCaps() {
+   (!capsK.classList.contains('down')) ? capsK.classList.add('down') : capsK.classList.remove('down');
+   capsK.classList.toggle('red');
+   fillKeys();
+}
+
+capsK.addEventListener('click', changeCaps);
+
+function fillKeys() {
+   if (localStorage.lang === 'en') {
+      for (let i = 0; i < rows.length; i++) {
+         for (let j = 0; j < rows[i].children.length; j++) {
+            if (capsK.classList.contains('down') && rows[i].children[j].textContent.length === 1) {
+               rows[i].children[j].innerHTML = dataButtons[i][j].key.en.toUpperCase();
+            } else if (rows[i].children[j].textContent.length === 1) {
+               rows[i].children[j].innerHTML = dataButtons[i][j].key.en;
+            }
+         }
+      }
+   } else {
+      for (let i = 0; i < rows.length; i++) {
+         for (let j = 0; j < rows[i].children.length; j++) {
+            if (capsK.classList.contains('down') && rows[i].children[j].textContent.length === 1) {
+               rows[i].children[j].innerHTML = dataButtons[i][j].key.ru.toUpperCase();
+            } else if (rows[i].children[j].textContent.length === 1) {
+               rows[i].children[j].innerHTML = dataButtons[i][j].key.ru;
+            }
+         }
+      }
+   }
+}
