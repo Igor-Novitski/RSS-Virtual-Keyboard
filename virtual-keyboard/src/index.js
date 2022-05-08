00114632d7beda61d37aa7d8c6acecee37d053fc
Igor-Novitski <pro100nke@gmail.com>
2022-05-08 17:21:24 +0300

    feat: add generation of DOM elements

--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -1,1 +1,17 @@
-console.log('start');
+import { createKeyboard } from './js/keyboard.js';
+import { createPageLayout } from './js/pageLayout.js';
+import dataButtons from './js/dataButtons.js';
+
+createPageLayout();
+createKeyboard();
+/*
+const keys = document.querySelectorAll('.key');
+
+function changeButton() {
+   keys.classList.add('active');
+}
+
+keys.addEventListener('click', changeButton);
+*/
+
+//document.body.innerHTML = '';
---
+++ virtual-keyboard/src/js/keyboard.js
@@ -0,0 +1,18 @@
+export function createKeyboard() {
+   const keyboard = document.querySelector(".keyboard");
+
+   const rowArr = [15, 15, 14, 13, 9]
+
+   for (let i = 0; i < rowArr.length; i++) {
+      const row = document.createElement('div');
+      row.className = 'row';
+      keyboard.append(row);
+      for (let j = 0; j < rowArr[i]; j++) {
+         const key = document.createElement('div');
+         key.className = 'key';
+         row.append(key);
+      }
+   }
+}
+
+
